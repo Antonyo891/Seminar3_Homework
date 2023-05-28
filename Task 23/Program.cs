e31ea711e2cd712d4f496a973ca5818aee9fb18b
Anton <B_atonyo_S@bk.ru>
2023-05-28 22:44:25 +0300

    Task 23 completed

---
+++ Task 23/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Clear();
+int Number = 0;
+while (Number<=1){
+    Console.WriteLine("Введите число N больше 1");
+    Number = int.Parse(Console.ReadLine());
+}
+for (int i = 1; i<=Number; i++)
+    System.Console.Write($"Число {i} в 3 степени = {Math.Pow(i,3)}; ");
+System.Console.WriteLine();
